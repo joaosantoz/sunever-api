28f4046f4a41f52ff0296f7259f8916056865240
joaosantoz <jvsantosdev@gmail.com>
2023-01-07 19:43:26 -0300

    first commit

---
+++ src/main/java/com/jovicsantos/suneverapi/SuneverApiApplication.java
@@ -0,0 +1,13 @@
+package com.jovicsantos.suneverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuneverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuneverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovicsantos/suneverapi/SuneverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovicsantos.suneverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuneverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
