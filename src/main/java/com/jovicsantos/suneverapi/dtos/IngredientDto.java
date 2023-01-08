e1098c497177697d6a0660c85384c8029dfcddd3
joaosantoz <jvsantosdev@gmail.com>
2023-01-08 01:55:16 -0300

    style: changing tab space

--- src/main/java/com/jovicsantos/suneverapi/dtos/IngredientDto.java
+++ src/main/java/com/jovicsantos/suneverapi/dtos/IngredientDto.java
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record IngredientDto(
-        @NotBlank String name,
-        @NotNull BigDecimal price,
-        @NotNull BigDecimal quantity_per_measure,
-        @NotNull UUID measurement_id) {
+		@NotBlank String name,
+		@NotNull BigDecimal price,
+		@NotNull BigDecimal quantity_per_measure,
+		@NotNull UUID measurement_id) {
 }
--- src/main/java/com/jovicsantos/suneverapi/dtos/MeasurementDto.java
+++ src/main/java/com/jovicsantos/suneverapi/dtos/MeasurementDto.java
@@ -5,6 +5,6 @@ import org.hibernate.validator.constraints.Length;
 import jakarta.validation.constraints.NotBlank;
 
 public record MeasurementDto(
-    @NotBlank String name,
-    @NotBlank @Length(max = 10) String abbreviation) {
+		@NotBlank String name,
+		@NotBlank @Length(max = 10) String abbreviation) {
 }
